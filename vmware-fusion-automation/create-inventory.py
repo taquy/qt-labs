1a8e990df0cc2fc0daa7f29d6ae57c58356f5d5e
Quy Ta <taquy.se@gmail.com>
2024-11-28 01:15:55 -0500

    refactor dir structure

--- vmware-fusion-automation/create-inventory.py
+++ vmware-fusion-automation/create-inventory.py
@@ -14,8 +14,6 @@ environment = Environment(loader=loader)
 
 VM_DIR = "/Users/qt/Virtual Machines.localized"
 
-Path("configs").mkdir(parents=True, exist_ok=True)
-
 # collect nodes and group nodes master prefix is `m`, worker prefix is `w`
 def create_inventories():
   master_nodes = {
@@ -96,7 +94,8 @@ def create_inventories():
     yaml.dump(inventories, outfile, default_flow_style=False)
     
   # save cluster nodes to file for later use: update host file
-  with open('configs/hosts_vars.yaml', 'w') as outfile:
+  Path("configs/ansible_vars").mkdir(parents=True, exist_ok=True)
+  with open('configs/ansible_vars/hosts.yaml', 'w') as outfile:
     yaml.dump({'hosts': host_file_records}, outfile, default_flow_style=False)
       
   return api_servers, cluster_nodes | other_nodes
@@ -105,12 +104,14 @@ def create_haproxy_config(api_servers):
   if not api_servers or len(api_servers) == 0:
     print('No API servers available')
   results_template = environment.get_template('templates/haproxy_config.j2')
-  with open('configs/haproxy_config', mode="w", encoding="utf-8") as results:
+  Path("configs/haproxy").mkdir(parents=True, exist_ok=True)
+  with open('configs/haproxy/haproxy.cfg', mode="w", encoding="utf-8") as results:
       results.write(results_template.render(api_servers=api_servers) + '\n')
       
-def get_cluster_token():
+def get_cluster_token(prefix):
   cluster_token = ''
-  token_file_name = 'cluster_token'
+  Path("configs/cluster_tokens").mkdir(parents=True, exist_ok=True)
+  token_file_name = f'configs/cluster_tokens/{prefix}'
   if os.path.isfile(token_file_name):
     print(f'Token file "{token_file_name}" found in path, using it as cluster token')
     token_file = open(token_file_name, 'r')
@@ -126,24 +127,27 @@ def get_cluster_token():
     token_file.close()
   return cluster_token
 
+def write_rke2_config(fn, **kwargs):
+  tpl = environment.get_template('templates/rke2_config.j2')
+  Path("configs/rke2_configs").mkdir(parents=True, exist_ok=True)
+  with open(f'configs/rke2_configs/{fn}', mode="w") as file:
+    file.write(tpl.render(kwargs) + '\n')
+
 def create_rke_config(cluster_nodes):
-  results_template = environment.get_template('templates/rke2_config.j2')
   lb_ips = cluster_nodes['load_balancers']
   lb_ip = lb_ips[0]
   backends = list(itertools.chain(*cluster_nodes['masters'].values())) + lb_ips
-  token = get_cluster_token()
+  shared_token = get_cluster_token('shared')
+  agent_token = get_cluster_token('agent')
   # create primary primary config
-  with open('configs/rke2_config_master_pri', mode="w") as file:
-    file.write(results_template.render(backends=backends, token=token,) + '\n')
+  write_rke2_config('primary_masters', backends=backends)
   # create secondary masters config
-  with open('configs/rke2_config_master_sec', mode="w") as file:
-    file.write(results_template.render(backends=backends, token=token, lb_ip=lb_ip) + '\n')
+  write_rke2_config('secondary_masters', backends=backends, lb_ip=lb_ip)
   # create workers config
   for worker_type in cluster_nodes['workers']:
-    with open(f'configs/rke2_config_{worker_type}_workers', mode="w") as file:
-      file.write(results_template.render(token=token, lb_ip=lb_ip, worker_type=worker_type) + '\n')
+    write_rke2_config(f'{worker_type}_workers', worker_type=worker_type, lb_ip=lb_ip)
   # save lb ip in a file to be used for generate rke config later
-  with open('configs/loadbalancer_ip', mode="w") as file:
+  with open('configs/haproxy/ip_address', mode="w") as file:
     file.write(lb_ip)
       
       
