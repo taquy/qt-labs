e318012ac20f632cd66d9319530cb4f1299b4251
Quy Ta <taquy.se@gmail.com>
2024-12-02 04:33:50 -0500

    add metallb

--- helm-deployments/2-network-system/install.sh
+++ helm-deployments/2-network-system/install.sh
@@ -7,7 +7,11 @@ kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 # install or upgrade the helm charts
 helm install \
   -f values/metallb.yaml \
+  -f values/traefik.yaml \
+  --namespace $ns $chart_name . ||
+  helm upgrade --namespace $ns \
+  -f values/metallb.yaml \
+  -f values/traefik.yaml \
   --namespace $ns $chart_name .
-  # helm upgrade --namespace $ns \
-  # # -f values/zabbix.yaml \
-  # $chart_name .
+
+kubectl apply -f manifests/post-install
