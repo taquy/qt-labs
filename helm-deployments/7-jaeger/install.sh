acd35f3052c54ba02f13958ae1f6d04c718f94cd
Quy Ta <taquy.se@gmail.com>
2024-12-09 17:33:07 -0500

    add opensearch

---
+++ helm-deployments/7-jaeger/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ns="jaeger"
+chart_name="jaeger"
+kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
+
+# install or upgrade the helm charts
+helm install \
+  -f values/jaeger.yaml \
+  -f values/zipkin.yaml \
+  --namespace $ns $chart_name . ||
+  helm upgrade --namespace $ns \
+  -f values/jaeger.yaml \
+  -f values/zipkin.yaml \
+  --namespace $ns $chart_name .
---
+++ vmware-fusion-automation/3-start-vmware-machines.py
@@ -0,0 +1,28 @@
+import yaml
+from pathlib import Path
+import os
+import shutil
+import os
+import subprocess
+
+with open("config.yaml") as stream:
+  global_config = yaml.safe_load(stream)
+  
+vm_dir = global_config.get('vm_dir')
+
+# remove lck files
+matching_files = []
+matching_dirs = []
+for root, dirs, files in os.walk(vm_dir):
+  for dir_name in dirs:
+    if 'lck' in dir_name:
+        shutil.rmtree(os.path.join(root, dir_name))
+  for file in files:
+    if file.endswith('lck'):
+      os.remove(os.path.join(root, file))
+
+# start vms
+for filepath in Path(vm_dir).glob('*vmwarevm'):
+  fn = filepath.absolute()
+  subprocess.run(['vmrun', 'start', fn, 'nogui'])
+
