fae85890fb6fcdb88a824132dab2b690f2891572
Quy Ta <taquy.se@gmail.com>
2024-12-10 00:12:46 -0500

    add cassandra

---
+++ helm-deployments/7-jaeger/subcharts/cassandra/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ns="jaeger"
+chart_name="jaeger-cassandra"
+kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
+
+# install or upgrade the helm charts
+helm install \
+  -f values/cassandra.yaml \
+  --namespace $ns $chart_name . ||
+  helm upgrade --namespace $ns \
+  -f values/cassandra.yaml \
+  --namespace $ns $chart_name .
