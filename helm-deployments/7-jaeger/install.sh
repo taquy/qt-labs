b60876523c2a080c430354d8a36cd80978ea02b2
Quy Ta <taquy.se@gmail.com>
2024-12-15 23:57:01 -0500

    add custom chart to create certs

--- helm-deployments/7-jaeger/install.sh
+++ helm-deployments/7-jaeger/install.sh
@@ -8,8 +8,10 @@ kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 helm install \
   -f values/jaeger.yaml \
   -f values/zipkin.yaml \
+  -f values/certs.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
   -f values/jaeger.yaml \
   -f values/zipkin.yaml \
+  -f values/certs.yaml \
   --namespace $ns $chart_name .
