3f9438a46aee6633e56bd33465add2d192c931c0
Quy Ta <taquy.se@gmail.com>
2024-12-18 06:14:59 -0500

    use bitnami solution for opensearch

--- helm-deployments/7-jaeger/sub-charts/opensearch/install.sh
+++ helm-deployments/7-jaeger/sub-charts/opensearch/install.sh
@@ -7,9 +7,7 @@ kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 # install or upgrade the helm charts
 helm install \
   -f values/opensearch.yaml \
-  -f values/opensearch-dashboards.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
   -f values/opensearch.yaml \
-  -f values/opensearch-dashboards.yaml \
   --namespace $ns $chart_name .
