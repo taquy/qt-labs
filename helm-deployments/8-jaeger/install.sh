632bf9f6d2fa1610f205dbf41c36c89b8083b2dc
Quy Ta <taquy.se@gmail.com>
2024-12-26 02:51:03 -0500

    add default zipkin and bitnami chart

--- helm-deployments/8-jaeger/install.sh
+++ helm-deployments/8-jaeger/install.sh
@@ -4,14 +4,17 @@ ns="jaeger"
 chart_name="jaeger"
 kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 
+chart_source=original
+# chart_source=bitnami
+
 # install or upgrade the helm charts
 helm install \
   -f values/jaeger.yaml \
-  -f values/zipkin.yaml \
+  -f values/${chart_source}/zipkin.yaml \
   -f values/certs.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
   -f values/jaeger.yaml \
-  -f values/zipkin.yaml \
+  -f values/${chart_source}/zipkin.yaml \
   -f values/certs.yaml \
   --namespace $ns $chart_name .
