37ac33b07e6ae444c7d0a6d64f623d6f1f7b1119
Quy Ta <taquy.se@gmail.com>
2024-12-29 16:05:06 -0500

    merge chart of otel collector and otel operator

--- helm-deployments/9-otel/1-otel-system/install.sh
+++ helm-deployments/9-otel/1-otel-system/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-ns="otel-collector"
-chart_name="otel-collector"
+ns="otel-system"
+chart_name="otel-system"
 kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 
 # install or upgrade the helm charts
 helm install \
+  -f values/opentelemetry-operator.yaml \
   -f values/opentelemetry-collector.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
+  -f values/opentelemetry-operator.yaml \
   -f values/opentelemetry-collector.yaml \
   --namespace $ns $chart_name .
