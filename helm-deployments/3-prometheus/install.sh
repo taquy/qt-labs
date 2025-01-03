74d53857015308d8530e7f2826b68e9b16e5b986
Quy Ta <taquy.se@gmail.com>
2025-01-03 11:11:15 -0500

    add alert manager and prom server

--- helm-deployments/3-prometheus/install.sh
+++ helm-deployments/3-prometheus/install.sh
@@ -6,8 +6,14 @@ kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 
 # install or upgrade the helm charts
 helm install \
+  -f values/kube-prometheus-stack.yaml \
   -f values/prometheus.yaml \
+  -f values/alertmanager.yaml \
+  -f values/alertmanager-snmp-notifier.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
+  -f values/kube-prometheus-stack.yaml \
   -f values/prometheus.yaml \
+  -f values/alertmanager.yaml \
+  -f values/alertmanager-snmp-notifier.yaml \
   --namespace $ns $chart_name .
