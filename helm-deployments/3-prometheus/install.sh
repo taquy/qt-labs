8bb4cb62b3041fb5471514998b977d78bb4f3c8e
Quy Ta <taquy.se@gmail.com>
2025-01-03 11:23:12 -0500

    remove snmp notifier

--- helm-deployments/3-prometheus/install.sh
+++ helm-deployments/3-prometheus/install.sh
@@ -9,11 +9,9 @@ helm install \
   -f values/kube-prometheus-stack.yaml \
   -f values/prometheus.yaml \
   -f values/alertmanager.yaml \
-  -f values/alertmanager-snmp-notifier.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
   -f values/kube-prometheus-stack.yaml \
   -f values/prometheus.yaml \
   -f values/alertmanager.yaml \
-  -f values/alertmanager-snmp-notifier.yaml \
   --namespace $ns $chart_name .
