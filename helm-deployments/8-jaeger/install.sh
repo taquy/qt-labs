34f6bf136ffab878732c5b1b77a479731c314281
Quy Ta <taquy.se@gmail.com>
2025-02-14 14:12:04 -0500

    reduce complexity in jaeger

--- helm-deployments/8-jaeger/install.sh
+++ helm-deployments/8-jaeger/install.sh
@@ -4,22 +4,20 @@ ns="jaeger"
 chart_name="jaeger"
 kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 
+kubectl apply -f manifests/
+
 # install or upgrade the helm charts
 helm install \
   -f values/cassandra.yaml \
   -f values/opensearch.yaml \
   -f values/jaeger.yaml \
-  -f values/jaeger-operator.yaml \
   -f values/zipkin.yaml \
-  -f values/certs.yaml \
   -f values/opentelemetry-collector.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
   -f values/cassandra.yaml \
   -f values/opensearch.yaml \
   -f values/jaeger.yaml \
-  -f values/jaeger-operator.yaml \
   -f values/zipkin.yaml \
-  -f values/certs.yaml \
   -f values/opentelemetry-collector.yaml \
   --namespace $ns $chart_name .
