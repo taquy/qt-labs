9ff4cd61dc9d1627c3906d1de0e48555aa8f13c6
Quy Ta <taquy.se@gmail.com>
2025-02-14 13:44:44 -0500

    add cassandra, opensearch

--- helm-deployments/8-jaeger/install.sh
+++ helm-deployments/8-jaeger/install.sh
@@ -6,6 +6,8 @@ kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 
 # install or upgrade the helm charts
 helm install \
+  -f values/cassandra.yaml \
+  -f values/opensearch.yaml \
   -f values/jaeger.yaml \
   -f values/jaeger-operator.yaml \
   -f values/zipkin.yaml \
@@ -13,6 +15,8 @@ helm install \
   -f values/opentelemetry-collector.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
+  -f values/cassandra.yaml \
+  -f values/opensearch.yaml \
   -f values/jaeger.yaml \
   -f values/jaeger-operator.yaml \
   -f values/zipkin.yaml \
