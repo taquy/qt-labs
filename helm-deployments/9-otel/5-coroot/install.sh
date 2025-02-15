d007d04f2d7ac6cfb9074ee666f9702e1d09e4e5
Quy Ta <taquy.se@gmail.com>
2025-02-15 12:50:53 -0500

    add otel collector endpoint

--- helm-deployments/9-otel/5-coroot/install.sh
+++ helm-deployments/9-otel/5-coroot/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-ns="mon-coroot"
+ns="coroot"
 chart_name="coroot"
 kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
 
 # install or upgrade the helm charts
 helm install \
   -f values/coroot.yaml \
+  -f values/coroot-operator.yaml \
   --namespace $ns $chart_name . ||
   helm upgrade --namespace $ns \
   -f values/coroot.yaml \
+  -f values/coroot-operator.yaml \
   --namespace $ns $chart_name .
