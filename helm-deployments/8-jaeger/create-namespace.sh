59c82b1b11ebbd5e3e68aed0f6548318a06bbe1d
Quy Ta <taquy.se@gmail.com>
2025-03-10 21:44:58 +0700

    add demo script

--- helm-deployments/8-jaeger/create-namespace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-declare -a namespaces=("jaeger")
-for (( i=0; i<${#namespaces[@]}; i++ ));
-do
-  ns="${namespaces[$i]}"
-  echo "Creating namesapce ${ns}"
-  kubectl get namespace | grep -q "^$ns " || kubectl create namespace $ns
-done
